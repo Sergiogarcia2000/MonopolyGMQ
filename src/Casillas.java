427364e0429d870c5aae15d6485e3c6adc6f648c
Sergiogarcia2000 <garciamayosergio@gmail.com>
2019-10-22 20:46:56 +0200

    Casillas y Tablero

--- src/Casillas.java
+++ src/Casillas.java
@@ -1,4 +1,28 @@
 
 public class Casillas {
 
+	private String casilla;
+	private int renta;
+	private int posicion;
+	
+	
+	public Casillas(String casilla, int renta, int posicion) {
+		
+		this.casilla = casilla;
+		this.renta = renta;
+		this.posicion = posicion;
+		
+	}
+
+	
+	public int pos() {
+		return this.posicion;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return casilla + " Renta: " + renta + " Posición: " + posicion;
+	}
+	
 }
--- src/Tablero.java
+++ src/Tablero.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tablero {
-
+	
+	private List<Casillas> table = new ArrayList<Casillas>();
+	private String[] lugares = {"Keller", "Aporpizza", "Essentia", "Incarlopsa", "El Abuelo", "Toria", "Xandros", "Mandioca", "Campanilla", "Massai", "La casa rosa", "AutoTarancon", "AutoGarcia","AutoRiansares","AutoSanCristobal", "AutoApto", "Towers", "Dolce Vita", "Mercadona", "Costa Rica", "La Juve", "MariaCristina", "Ferial", "HalliBaba", "China Maria", "Tabu", "Manao", "Zoo", "Martina", "Versalles", "Eclipse", "Veyllon", "Girasoles","Librado","Quiles", "Codere"};
+	
+	private int Precio = 500;
+	
+	public Tablero() {
+		
+		for (int i = 0; i<lugares.length; i++) {
+			table.add(new Casillas(lugares[i], Precio, i));
+			Precio = Precio + 100;
+		}
+		
+	
+	}
+	
+	public void info(int pos) {
+		
+		for (Casillas c: table) {
+			if (c.pos() == pos) {
+				System.out.println(c);
+			}
+		}
+		
+	}
+	
 }
