33f3676e0b385b231c54fe8209bdad5a8d782f64
PelicanoSalvaje <daniel.clase.2019@gmail.com>
2019-10-24 20:40:10 +0200

    Añadido las cuentas del jugador cuando cae en una casilla ya comprada

--- src/Jugadores.java
+++ src/Jugadores.java
@@ -4,11 +4,14 @@ public class Jugadores {
 	private String nombre;
 	private int dinero;
 	private int posicion;
+	private int [] posicionesEnPropiedad = new int[40];
+	private int indexJ;
 	//Constructor
 	public Jugadores(String nombre) {
 		this.nombre=nombre;
 		dinero = 15000;
 		posicion = 0;
+		indexJ = -1;
 	}
 	//Setter para editar el dinero
 	public void editarDinero(int dinero) {
@@ -17,6 +20,16 @@ public class Jugadores {
 	//setter para editar posicion
 	public void editarPosicion(int posicion) {
 		this.posicion = posicion;
+	}
+	public void addCasilla(int posicion, int index) {
+		posicionesEnPropiedad[index] = posicion;	
+	}
+	public int dameIndex() {
+		indexJ+=1;
+		return indexJ;
+	}
+	public int dameCasillaEnPropiedad(int index) {
+		return posicionesEnPropiedad[index];
 	}
 	//Getter para obtener el dinero
 	public int dameDinero() {
--- src/main.java
+++ src/main.java
@@ -17,6 +17,9 @@ public class main {
 		for (int i = 0 ; i <= numJugadores-1 ; i++) {
 			
 			jugador[i] = new Jugadores(JOptionPane.showInputDialog("Nombre del jugador " + (i+1)));
+			//for(int j = 0; j <= 39;j++) {
+				//jugador[i].addBadCasilla(j);
+			//}
 		}
 			//elige aleatoriamente al primer jugador en empezar
 		dadoComienzo =(int)Math.random()*numJugadores;
@@ -34,26 +37,39 @@ public class main {
 			jugador[dadoComienzo].editarPosicion(dado.dameResulDados()+jugador[dadoComienzo].damePosicion());
 			//si la casilla es superior a 40 empieza de nuevo
 			if(jugador[dadoComienzo].damePosicion() >= 40) {
-				jugador[dadoComienzo].editarPosicion(dado.dameResulDados());
+				
+				jugador[dadoComienzo].editarPosicion( jugador[dadoComienzo].damePosicion() - 40 );
 			}
 			//consultar datos de la posicion
 			tablero.info(jugador[dadoComienzo].damePosicion());
 			
 			jugador[dadoComienzo].damePosicion();
 			//comprobacion si esta comprada la posicion
-			if(tablero.comprada(jugador[dadoComienzo].damePosicion()) == false) {
+			if(tablero.comprada(jugador[dadoComienzo].damePosicion())) {
 				
-				int comprar = JOptionPane.showOptionDialog(null, "¿Quiere comprar la casilla?", "Casilla sin comprar", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null,new Object[] {"Si", "No"}, null);	 
 				
-				if(comprar == 0) {
+				for (int i = 0 ; i <= numJugadores-1 ; i++) {
+					for(int j = 0;j <= 39;j++) {
+						if (jugador[i].dameCasillaEnPropiedad(j) == jugador[dadoComienzo].damePosicion()){
+							jugador[i].editarDinero(jugador[i].dameDinero()+ tablero.renta(dadoComienzo));
+							jugador[dadoComienzo].editarDinero(jugador[dadoComienzo].dameDinero() - tablero.renta(dadoComienzo));
+						}
+					}
 					
-					jugador[dadoComienzo].editarDinero(jugador[dadoComienzo].dameDinero()- tablero.renta(jugador[dadoComienzo].damePosicion()));
+				}
+				System.out.println("Has pagado de renta " + tablero.renta(jugador[dadoComienzo].damePosicion()) + " Te queda " + jugador[dadoComienzo].dameDinero() + "€");	
+				}else {
+					int comprar = JOptionPane.showOptionDialog(null, "¿Quiere comprar la casilla?", "Casilla sin comprar", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null,new Object[] {"Si", "No"}, null);	 
+				
+					if(comprar == 0) {
 					
-					tablero.editarComprada(jugador[dadoComienzo].damePosicion());
+						jugador[dadoComienzo].editarDinero(jugador[dadoComienzo].dameDinero()- tablero.renta(jugador[dadoComienzo].damePosicion()));
 					
-					System.out.println("Comprada");
-					System.out.println("Te queda "+ jugador[dadoComienzo].dameDinero() + "€");
-						
+						tablero.editarComprada(jugador[dadoComienzo].damePosicion());
+						jugador[dadoComienzo].addCasilla(jugador[dadoComienzo].damePosicion(),jugador[dadoComienzo].dameIndex());
+					
+						System.out.println("Comprada");
+						System.out.println("Te queda "+ jugador[dadoComienzo].dameDinero() + "€");
 				}
 			}
 			if(dadoComienzo < numJugadores-1) {
